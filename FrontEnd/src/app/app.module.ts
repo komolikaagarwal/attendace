0e6543e9447e7794b4f94a1c9290b97f8a0da685
tinasoni77 <sonitina973@gmail.com>
2020-09-11 14:39:36 +0530

    edited src

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -4,16 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
 
-    DashboardComponent,
+    HeaderComponent,
 
-    HeaderComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
